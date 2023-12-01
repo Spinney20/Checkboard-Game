c922149532b7ac36181b728a7203314a6fa0e16c
Dobre Andrei-Teodor <116495955+Spinney20@users.noreply.github.com>
2023-12-01 11:36:07 +0200

    Added functii_gigel.c and functii_gigel.h

---
+++ functii_gigel.h
@@ -0,0 +1,8 @@
+//Dobre Andrei-Teodor 314CA
+#pragma once
+#include <stdio.h>
+
+int pozitiv(long long x);
+int micsorare(int n, long long input);
+int marire(int n, long long input);
+void afisare_excel(int y);
